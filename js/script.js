7cadfe202e0acf6ab05eca25e9cc17a39ee587c7
antoniovenezia <antoniovenezia00@gmail.com>
2021-05-28 17:24:42 +0200

    js-biglietto-treno

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 var x = prompt("Quanti km hai intenzione di percorrere?");
 var f = prompt("In che anno sei nato?");
 
+var w = document.getElementById("price");
 
-var g = parseInt(f);
-var pricetokm = parseInt(x);
-
-
-var y = 2021 - g;
-var z = pricetokm * 0.21;
+var km = parseInt(x);
+var year = parseInt(f);
 
 
-if (y < 18) {
+var priceKm = km * 0.21;
+var age = 2021 - year;
+
+
+if (age < 18) {
        
-    document.getElementById("price").innerHTML = (z - (z * 20 / 100)).toFixed(2);
+    w.innerHTML = (priceKm - (priceKm * 20 / 100)).toFixed(2);
 
-} else if (y > 65) {
+} else if (age > 65) {
     
-    document.getElementById("price").innerHTML = (z - (z * 40 / 100)).toFixed(2);
+    w.innerHTML = (priceKm - (priceKm * 40 / 100)).toFixed(2);
     
 } else {
 
-    document.getElementById("price").innerHTML = z.toFixed(2);
+    w.innerHTML = priceKm.toFixed(2);
 }
