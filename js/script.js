4842c51e81998c0f1e54a9736dd1c71959e6ee8e
antoniovenezia <antoniovenezia00@gmail.com>
2021-05-28 17:26:42 +0200

    js-biglietto-treno+bonus

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var x = prompt("Quanti km hai intenzione di percorrere?");
-var f = prompt("In che anno sei nato?");
+var y = prompt("In che anno sei nato?");
 
-var w = document.getElementById("price");
+var z = document.getElementById("price");
 
 var km = parseInt(x);
-var year = parseInt(f);
+var year = parseInt(y);
 
 
 var priceKm = km * 0.21;
@@ -13,13 +13,13 @@ var age = 2021 - year;
 
 if (age < 18) {
        
-    w.innerHTML = (priceKm - (priceKm * 20 / 100)).toFixed(2);
+    z.innerHTML = (priceKm - (priceKm * 20 / 100)).toFixed(2);
 
 } else if (age > 65) {
     
-    w.innerHTML = (priceKm - (priceKm * 40 / 100)).toFixed(2);
+    z.innerHTML = (priceKm - (priceKm * 40 / 100)).toFixed(2);
     
 } else {
 
-    w.innerHTML = priceKm.toFixed(2);
+    z.innerHTML = priceKm.toFixed(2);
 }
